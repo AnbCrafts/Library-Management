2fd8a55010b7af65c5e6bde7bf24a060bb7cbdfb
Anubhaw Kumar Gupta <anubhaw22052003@gmail.com>
2025-04-14 13:39:29 +0530

    Update Books.jsx

--- frontend/src/User/Pages/Books.jsx
+++ frontend/src/User/Pages/Books.jsx
@@ -11,7 +11,7 @@ import { AuthContext } from "../../ContextAPI/AuthContext";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
-  const backendURI = "http://localhost:7000";
+  const backendURI = "https://backend-v1bd.onrender.com";
   const navigate = useNavigate();
   const location = useLocation();
   const{userId} = useParams();
