9c5f7a16eab22570bfe6fbb86e42034a2ef24f65
Anubhaw Kumar Gupta <anubhaw22052003@gmail.com>
2025-04-14 13:34:18 +0530

    Update AddBlog.jsx

--- frontend/src/Admin/Pages/AddBlog.jsx
+++ frontend/src/Admin/Pages/AddBlog.jsx
@@ -10,7 +10,7 @@ import { useNavigate, useParams } from "react-router-dom";
 const AddBlog = () => {
   const [coverImgList, setCoverImgList] = useState([]);
   const [authorImgList, setAuthorImgList] = useState([]);
-  const backendURI = "http://localhost:7000";
+  const backendURI = "https://backend-v1bd.onrender.com";
   const navigate = useNavigate();
   const { adminId } = useParams(); 
  
